bcfae5199bb72200357f2550fc6661c397f9fe14
jiang7462582 <jiang7462582@126.com>
2016-02-29 14:35:40 +0800

    Update Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,27 +4,13 @@ import javax.inject.Inject
 
 import models.Mq
 import play.api._
-import play.api.libs.ws.WSClient
 import play.api.mvc._
 
 
 
-class Application @Inject() (ws: WSClient,mq:Mq) extends Controller {
-
+class Application @Inject() (mq:Mq) extends Controller {
   def index = Action {
-
-      //val s = new MyComponent(ws)(1)
       mq.send("helloWorld222")
-
     Ok(views.html.index("Your new application is ready."))
   }
-
-}
-
-
-
-
-class MyComponent @Inject() (ws: WSClient)(x:Int) {
-    def s = ws.url("www.baidu.com").get()
-    // ...
 }
